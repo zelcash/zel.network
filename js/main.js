2ae4c8a2723d4e567091b218e52023a23a60334a
Lumiboy <tha_lumi@hotmail.com>
2018-06-20 12:05:03 +0200

    Clean up

--- js/main.js
+++ js/main.js
@@ -1,22 +1,4 @@
-/* =================================
-------------------------------------
-	Cryptocurrency - Landing Page Template
-	Version: 1.0
- ------------------------------------ 
- ====================================*/
-
-
 'use strict';
-
-
-$(window).on('load', function() {
-	/*------------------
-		Preloder
-	--------------------*/
-	$(".loader").fadeOut(); 
-	$("#preloder").delay(400).fadeOut("slow");
-
-});
 
 (function($) {
 
@@ -27,53 +9,6 @@ $(window).on('load', function() {
 		$('.main-menu').slideToggle(400);
 		event.preventDefault();
 	});
-
-
-	/*------------------
-		Background set
-	--------------------*/
-	$('.set-bg').each(function() {
-		var bg = $(this).data('setbg');
-		$(this).css('background-image', 'url(' + bg + ')');
-	});
-
-	
-	/*------------------
-		Review
-	--------------------*/
-	var review_meta = $(".review-meta-slider");
-    var review_text = $(".review-text-slider");
-
-
-    review_text.on('changed.owl.carousel', function(event) {
-		review_meta.trigger('next.owl.carousel');
-	});
-
-	review_meta.owlCarousel({
-		loop: true,
-		nav: false,
-		dots: true,
-		items: 3,
-		center: true,
-		margin: 20,
-		autoplay: true,
-		mouseDrag: false,
-	});
-
-
-	review_text.owlCarousel({
-		loop: true,
-		nav: true,
-		dots: false,
-		items: 1,
-		margin: 20,
-		autoplay: true,
-		navText: ['<i class="ti-angle-left"><i>', '<i class="ti-angle-right"><i>'],
-		animateOut: 'fadeOutDown',
-    	animateIn: 'fadeInDown',
-	});
-
-
 
 	 /*------------------
 		Contact Form
@@ -87,6 +22,4 @@ $(window).on('load', function() {
         }
     });
 
-
 })(jQuery);
-
