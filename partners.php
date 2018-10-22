50f5eacbf49c092cc10ae7ab066236e33762e8ff
Lumi <lumizelcash@gmail.com>
2018-10-22 18:09:55 +0200

    Include instead of rewriting
    Going with PHP instead, easier to change footer and etc.

---
+++ partners.php
@@ -0,0 +1,48 @@
+<section id="team" class="team-section spad text-center">
+    <div class="container">
+        <div data-aos="fade-down" data-aos-duration="2000" class="section-title text-center">
+            <h2 class="lmeetourpartners">Meet Our Partners</h2>
+            <p>Zel has partnered with industry leaders</p>
+        </div>
+    </div>
+    <div class="team-members clearfix">
+        <!-- Partner -->
+        <div data-aos="zoom-in" data-aos-duration="500" class="member">
+            <div class="member-text">
+                <div class="member-img set-bg" data-setbg="./img/team/PulseLab.png"></div>
+                <h2>Pulse Lab</h2>
+                <span>Accelerator Group</span>
+            </div>
+            <div class="member-social">
+                <a href="https://pulselab.io/" target="_blank"><i class="fas fa-globe"></i></a>
+            </div>
+            <div class="member-info">
+                <div class="member-img mf set-bg" data-setbg="./img/team/PulseLab.png"></div>
+                <div class="member-meta">
+                    <h2>Pulse Lab</h2>
+                    <span>Accelerator Group</span>
+                </div>
+                <p>Pulse Lab is a blockchain-focused accelerator providing expert consultation in legal structure, finance, technology, and tokenomics for paradigm-shifting projects.</p>
+            </div>
+        </div>
+        <!-- Partner -->
+        <div data-aos="zoom-in" data-aos-duration="500" class="member">
+            <div class="member-text">
+                <div class="member-img set-bg" data-setbg="./img/team/IC.png"></div>
+                <h2>Iterative Capital</h2>
+                <span>Institutional capital growth partner</span>
+            </div>
+            <div class="member-social">
+                <a href="https://i2trading.com/" target="_blank"><i class="fas fa-globe"></i></a>
+            </div>
+            <div class="member-info">
+                <div class="member-img mf set-bg" data-setbg="./img/team/IC.png"></div>
+                <div class="member-meta">
+                    <h2>Iterative Capital</h2>
+                    <span>Institutional capital growth partner</span>
+                </div>
+                <p>Over-the-counter cryptocurrency liquidity specialists.</p>
+            </div>
+        </div>
+    </div>
+</section>
