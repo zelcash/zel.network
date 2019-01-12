0cdc2c545c9574b4bf9d300cb72ad2a41e8478f2
trial123Zel <38964930+trial123Zel@users.noreply.github.com>
2019-01-12 13:22:06 -0800

    remove copay links

--- downloads.php
+++ downloads.php
@@ -23,19 +23,6 @@
                     <figure class="downloads-icon">
                         <img src="./img/logo-zelcash-round.svg" alt="#">
                     </figure>
-                    <h4>ZelCash Copay Wallet</h4>
-                        <a href="https://github.com/zelcash/zelcash-copay-wallet/releases/download/v10.1.0/ZelCash_Wallet_Windows.zip"><img src="./img/windows.svg" alt="ZelCash Copay for Windows"></a>
-                        <a href="https://github.com/zelcash/zelcash-copay-wallet/releases/download/v10.1.0/ZelCash_Wallet_macOS.dmg"><img src="./img/macos.svg" alt="ZelCash Copay for macOS"></a>
-                        <a href="https://github.com/zelcash/zelcash-copay-wallet/releases/download/v10.1.0/ZelCash_Wallet_Linux.zip"><img src="./img/linux.svg" alt="ZelCash Copay for Linux"></a>
-                        <a href="https://play.google.com/store/apps/details?id=com.zelcash.wallet" target="_blank"><img src="./img/android.svg" alt="ZelCash Copay for Android"></a>
-                        <a href="https://itunes.apple.com/app/zelcash-wallet/id1425452511?mt=8" target="_blank"><img src="./img/apple.svg" alt="ZelCash Copay for iOS"></a>
-                    </div>
-            </div>
-            <div data-aos="zoom-in" data-aos-duration="500" class="col-md-4 downloads">
-                <div class="downloads-step">
-                    <figure class="downloads-icon">
-                        <img src="./img/logo-zelcash-round.svg" alt="#">
-                    </figure>
                     <h4>ZelCash Swing Wallet</h4>
                         <a href="https://github.com/zelcash/zelcash-swing-wallet/releases"><img src="./img/windows.svg" alt="ZelCash Swing for Windows"></a>
                         <a href="https://github.com/zelcash/zelcash-swing-wallet/releases"><img src="./img/macos.svg" alt="ZelCash Swing for macOS"></a>
