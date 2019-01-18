b055bb8bccccc2cf37fd95ebf0741005f75347c0
trial123Zel <38964930+trial123Zel@users.noreply.github.com>
2019-01-18 14:16:53 -0800

    zelnodes page v1

--- zelnodes/index.php
+++ zelnodes/index.php
@@ -71,7 +71,7 @@
 	<!-- Page info section -->
 	<section class="page-info-section" data-paroller-factor="0.5">
 		<div class="container">
-			<h2 data-aos="fade-down" data-aos-duration="2000">ZelNodes</h2>
+			<h1 data-aos="fade-down" data-aos-duration="2000">ZelNodes</h1>
 			<div data-aos="zoom-in" data-aos-duration="500" class="site-beradcamb">
 				<a href="<?php echo $base_url;?>/" class="lhome">Home </a>
 				<i class="fa fa-angle-right"></i><span>ZelNodes</span>
@@ -96,8 +96,16 @@
 						<div class="minutes"></div>
 						<div class="seconds"></div>
 					</div>
-					<h5>ZelNodes release on height: 300,000</h5>
-					<h5>Estimated releasedate for ZelNodes: xth x 2019</h5>
+					<h5>ZelNodes release on height: 278,600</h5>
+					<h5>Estimated release date of  ZelNodes Source: 31st January 2019</h5>
+					<h5>Estimated launch of ZelNodes/Rewards: 21st February 2019</h5>
+					<br>
+					<br>
+					<div>
+						<a href="/img/ZelNode_Infographic_v2.png" class="site-btn sb-gradients mt-5" target="_blank">Infographic</a>
+						<a href="https://medium.com/@ZelOfficial/zelnodes-the-decentralized-scalable-high-availability-computing-network-57c1b4245fbd" class="site-btn sb-gradients mt-5" target="_blank">ZelNodes Details</a>
+						<a href="https://medium.com/@ZelOfficial/zelnodes-zel-partners-with-service-providers-for-rollout-c4b0aa8895e2" class="site-btn sb-gradients mt-5" target="_blank">VPS Partnerships</a>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -109,15 +117,31 @@
 		<div class="container">
 			<div class="row">
 				<div data-aos="fade-right" data-aos-duration="1000"  class="col-lg-6 offset-lg-0 about-text">
-					<h2>Info</h2>
-					<h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut rem voluptatum iusto aperiam necessitatibus porro non voluptatem dignissimos autem, excepturi obcaecati nulla fuga ratione sapiente? Commodi deleniti qui animi aut?</h5>
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae dolorem iste ratione reiciendis ducimus sequi? Molestiae, fugit. Veritatis, sapiente veniam laborum aliquam dicta excepturi itaque maxime, hic molestiae, consequatur cupiditate.</p>
+					<h2>ZelNodes Info</h2>
+					<h5>
+						<ul>
+							<li>Scalable, decentralized computational network</li>
+							<li>3 tiers of ZelNodes <a href="#specs">System Specs</a></li>
+							<li>Node ownership & physical location fully decentralized</li>
+							<li>Run via VPS or your own bare metal</li>
+							<li>Earn ZelCash 24 hours a day <a href="#rewards">Operator Rewards</a></li>
+						</ul>
+					</h5>
+					<p>Hardware will be supplied by node operators, who in turn are rewarded in ZelCash for supplying the network with dedicated processing power. There are multiple options for supplying computing power, including renting from a VPS provider, renting a dedicated server to host multiple ZelNodes, or purchasing a server outright and hosting it in a data center or well-equipped home/office.</p>
+					<p><b>Note: </b>The monthly cost to either rent or run dedicated hardware is significantly higher than earlier projects' masternodes and should be researched thoroughly by a potential node operator. ZelNodes do much more than simply validate transactions.</p>
 				</div>
 				<div data-aos="fade-left" data-aos-duration="1000"  class="col-lg-6 offset-lg-0 about-text">
 					<h2>Highlights</h2>
 					<ul>
-						<li>Scalable</li>
-						<li>ZelNodes <a rel="tooltip" title="3-Tier decentralized network"><i class="fas fa-info-circle"></i></a></li>
+						<li>High-availability</li><br>
+						<li>Hundreds of Nodes at launch <a rel="tooltip" title="Launch expectations based on rich list, trading volumes, community outreach and questionaires, dedicated hardware hosters, and other factors. This is an estimate."><i class="fas fa-info-circle"></i></a></li><br>
+						<li># of Nodes not artificially limited <a rel="tooltip" title="Nodes at any tier are only limited by what the public dictates is acceptable rewards and the maximum supply of ZelCash. There is no upper limit of any tier implemented by the project."><i class="fas fa-info-circle"></i></a></li><br>
+						<li>VPS discounts through our partners</li><br>
+						<li>Support via Discord for Node operators</li><br>
+						<li>3 levels of ZelNode ownership</li><br>
+						<li>Nodes will power future dapps & smart contracts</li><br>
+						<li>Source code available January 31, 2019</li><br>
+						<li>Network upgrade & Nodes launch February 21, 2019 <a href="#launch">Details</a></li><br>
 					</ul>
 				</div>
 			</div>
@@ -126,11 +150,12 @@
 	<!-- About section end -->
 
 	<!-- ZelNodes -->
+	<a name="specs"></a>
 	<section class="features-section spad gradient-bg">
 		<div class="container text-white">
 			<div data-aos="fade-down" data-aos-duration="1000" class="section-title text-center">
 				<h2>ZelNodes</h2>
-				<p>3-tiers</p>
+				<h3>3 tiers of computational nodes, allowing anyone to participate</h3>
 			</div>
 			<div class="row">
 				<div data-aos="zoom-in" data-aos-duration="500" class="col-md-6 col-lg-4 feature">
@@ -178,13 +203,16 @@
 	<!-- ZelNodes end -->
 
 	<!-- Reward Structure -->
+	<a name="rewards"></a>
 	<section id="about" class="about-section spad">
 		<div class="container">
 			<div data-aos="fade-right" data-aos-duration="1000" class="row">
 				<div class="col-lg-6 offset-lg-6 about-text">
 					<h2>Reward Structure</h2>
-					<h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore voluptatibus velit nobis mollitia repellendus accusamus, rerum nihil repellat totam, dolorum autem animi maiores. Doloremque obcaecati hic nihil totam, recusandae excepturi.</h5>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus tenetur nam, magnam error enim nisi beatae ea aliquam soluta excepturi voluptas fugiat perferendis voluptate adipisci eum eligendi consequuntur amet harum.</p>
+					<h5>The block reward is split 75% POW / 25% ZelNode operators, for each and every block. The rewards for ZelNode ownership is designed to be stable and long-term. This will not be a "10,000% ROI masternode" project that dies 1 week after launch. We promote ZelCash accumulation, not dumping.</h5>
+					<p>Of the 25% ZelNode block share, 15% goes to Basics, 25% to Supers, and 60% to BAMFs. Each chunk of rewards is split among the operators at that tier, e.g. if there are 100 BAMF ZelNodes in operation, each operator would receive:</p>
+					<p><b>150 * [(0.25 * 0.60) / 100] = 0.225 ZelCash every ~2 minutes</b></p>
+					<p>(150 Zel per block, 2 minute target find time).</p>
 				</div>
 			</div>
 			<div data-aos="fade-left" data-aos-duration="1000" class="about-img">
@@ -213,7 +241,21 @@
 		</div>
 	</section>
 	<!-- Reward Structure end -->
-
+	<!-- Launch process start -->
+	<section id="launch" class="about-section spad gradient-bg">
+		<div class="container text-white">
+			<div data-aos="fade-left" data-aos-duration="1000" class="row">
+				<div class="about-text">
+					<a name="launch"></a>
+					<h2>Launch Details</h2>
+					<br><br><h5>ZelNodes rollout requires both a ZelCash network upgrade and the ZelNodes daemon set up and running on your VPS/dedicated server. To minimize launch issues, these large items cannot be performed on the same day, and time to correct any errors and support node operators needs to be set aside.</h5><br>
+					<h5>There will be a 3-week buffer between code release and ZelNode rewards turning on to allow time for the network upgrade and operators to stand up their nodes, implement server security, and network testing.</h5><br>
+					<h3>During this time, we will also implement features to set up your control wallet using ZelCore light addresses and have written and video instructions with install scripts available. This will be hugely beneficial to operators ease-of-use as typically masternode projects require a full node payments wallet with fully synced chain data.</h3>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Reward Structure end -->
     <?php include '../footer.php';?>
 
     <?php include '../scripts.php';?>
@@ -238,7 +280,7 @@
 		$sec = $('.seconds');
 
 	$.getJSON('https://zel.coinblockers.com/api/stats', function(data) {
-		let forkHeight=300000;
+		let forkHeight=278600;
 		let blockTime=120;
 		let currentHeight = `${data.pools.zelcash.poolStats.networkBlocks}`
 		let blocksLeft = forkHeight - currentHeight;
