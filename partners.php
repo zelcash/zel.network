4256ef02303297a4e69699bf6a0cfc28f89f4d23
Lumiboy <tha_lumi@hotmail.com>
2019-01-25 23:16:57 +0100

    All partners and fix smaller card

--- partners.php
+++ partners.php
@@ -49,7 +49,7 @@
             <div class="member-text">
                 <div class="member-img set-bg" data-setbg="./img/team/DigitalOcean.png"></div>
                 <h2>DigitalOcean</h2>
-                <span>VPS Partner</span>
+                <span>DO Info</span>
             </div>
             <div class="member-social">
                 <a href="https://digitalocean.com" target="_blank"><i class="fas fa-globe"></i></a>
@@ -58,7 +58,26 @@
                 <div class="member-img mf set-bg" data-setbg="./img/team/DigitalOcean.png"></div>
                 <div class="member-meta">
                     <h2>DigitalOcean</h2>
-                    <span>VPS Partner</span>
+                    <span>DO Info</span>
+                </div>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </div>
+        </div>
+        <!-- Partner -->
+        <div data-aos="zoom-in" data-aos-duration="500" class="member">
+            <div class="member-text">
+                <div class="member-img set-bg" data-setbg="./img/team/AWS.png"></div>
+                <h2>AWS</h2>
+                <span>AWS Info</span>
+            </div>
+            <div class="member-social">
+                <a href="https://aws.amazon.com" target="_blank"><i class="fas fa-globe"></i></a>
+            </div>
+            <div class="member-info">
+                <div class="member-img mf set-bg" data-setbg="./img/team/AWS.png"></div>
+                <div class="member-meta">
+                    <h2>AWS</h2>
+                    <span>AWS Info</span>
                 </div>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
             </div>
@@ -74,11 +93,11 @@
     </div>
     <div class="team-members clearfix">
         <!-- Affiliate -->
-        <div data-aos="zoom-in" data-aos-duration="500" class="member">
+        <div data-aos="zoom-in" data-aos-duration="500" class="member smaller">
             <div class="member-text">
                 <div class="member-img set-bg" data-setbg="./img/team/MakerDAO.png"></div>
                 <h2>MakerDAO</h2>
-                <span>Text</span>
+                <span>MakerDAO Info</span>
             </div>
             <div class="member-social">
                 <a href="https://makerdao.com" target="_blank"><i class="fas fa-globe"></i></a>
@@ -87,17 +106,17 @@
                 <div class="member-img mf set-bg" data-setbg="./img/team/MakerDAO.png"></div>
                 <div class="member-meta">
                     <h2>MakerDAO</h2>
-                    <span>Text</span>
+                    <span>MakerDAO Info</span>
                 </div>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
             </div>
         </div>
         <!-- Affiliate -->
-        <div data-aos="zoom-in" data-aos-duration="500" class="member">
+        <div data-aos="zoom-in" data-aos-duration="500" class="member smaller">
             <div class="member-text">
                 <div class="member-img set-bg" data-setbg="./img/team/Gemini.png"></div>
                 <h2>Gemini Dollar</h2>
-                <span>Text</span>
+                <span>Gemini Info</span>
             </div>
             <div class="member-social">
                 <a href="https://gemini.com" target="_blank"><i class="fas fa-globe"></i></a>
@@ -106,7 +125,102 @@
                 <div class="member-img mf set-bg" data-setbg="./img/team/Gemini.png"></div>
                 <div class="member-meta">
                     <h2>Gemini Dollar</h2>
-                    <span>Text</span>
+                    <span>Gemini Info</span>
+                </div>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </div>
+        </div>
+        <!-- Affiliate -->
+        <div data-aos="zoom-in" data-aos-duration="500" class="member smaller">
+            <div class="member-text">
+                <div class="member-img set-bg" data-setbg="./img/team/Paxos.png"></div>
+                <h2>Paxos Standard</h2>
+                <span>Paxos Info</span>
+            </div>
+            <div class="member-social">
+                <a href="https://paxos.com" target="_blank"><i class="fas fa-globe"></i></a>
+            </div>
+            <div class="member-info">
+                <div class="member-img mf set-bg" data-setbg="./img/team/Paxos.png"></div>
+                <div class="member-meta">
+                    <h2>Paxos Standard</h2>
+                    <span>Paxos Info</span>
+                </div>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </div>
+        </div>
+        <!-- Affiliate -->
+        <div data-aos="zoom-in" data-aos-duration="500" class="member smaller">
+            <div class="member-text">
+                <div class="member-img set-bg" data-setbg="./img/team/CoinSwitch.png"></div>
+                <h2>CoinSwitch</h2>
+                <span>CoinSwitch Info</span>
+            </div>
+            <div class="member-social">
+                <a href="https://coinswitch.co" target="_blank"><i class="fas fa-globe"></i></a>
+            </div>
+            <div class="member-info">
+                <div class="member-img mf set-bg" data-setbg="./img/team/CoinSwitch.png"></div>
+                <div class="member-meta">
+                    <h2>CoinSwitch</h2>
+                    <span>CoinSwitch Info</span>
+                </div>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </div>
+        </div>
+        <!-- Affiliate -->
+        <div data-aos="zoom-in" data-aos-duration="500" class="member smaller">
+            <div class="member-text">
+                <div class="member-img set-bg" data-setbg="./img/team/Changelly.png"></div>
+                <h2>Changelly</h2>
+                <span>Changelly Info</span>
+            </div>
+            <div class="member-social">
+                <a href="https://changelly.com" target="_blank"><i class="fas fa-globe"></i></a>
+            </div>
+            <div class="member-info">
+                <div class="member-img mf set-bg" data-setbg="./img/team/Changelly.png"></div>
+                <div class="member-meta">
+                    <h2>Changelly</h2>
+                    <span>Changelly Info</span>
+                </div>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </div>
+        </div>
+        <!-- Affiliate -->
+        <div data-aos="zoom-in" data-aos-duration="500" class="member smaller">
+            <div class="member-text">
+                <div class="member-img set-bg" data-setbg="./img/team/ShapeShift.png"></div>
+                <h2>ShapeShift</h2>
+                <span>ShapeShift Info</span>
+            </div>
+            <div class="member-social">
+                <a href="https://shapeshift.io" target="_blank"><i class="fas fa-globe"></i></a>
+            </div>
+            <div class="member-info">
+                <div class="member-img mf set-bg" data-setbg="./img/team/ShapeShift.png"></div>
+                <div class="member-meta">
+                    <h2>ShapeShift</h2>
+                    <span>ShapeShift Info</span>
+                </div>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </div>
+        </div>
+        <!-- Affiliate -->
+        <div data-aos="zoom-in" data-aos-duration="500" class="member smaller">
+            <div class="member-text">
+                <div class="member-img set-bg" data-setbg="./img/team/Kyber.png"></div>
+                <h2>Kyber</h2>
+                <span>Kyber Info</span>
+            </div>
+            <div class="member-social">
+                <a href="https://kyber.network" target="_blank"><i class="fas fa-globe"></i></a>
+            </div>
+            <div class="member-info">
+                <div class="member-img mf set-bg" data-setbg="./img/team/Kyber.png"></div>
+                <div class="member-meta">
+                    <h2>Kyber</h2>
+                    <span>Kyber Info</span>
                 </div>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
             </div>
