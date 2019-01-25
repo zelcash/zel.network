4cc6f27dacfb81d239841d410c415592edcf2b03
trial123Zel <38964930+trial123Zel@users.noreply.github.com>
2019-01-25 15:33:53 -0800

    partners&affiliations v2

--- partners.php
+++ partners.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div data-aos="fade-down" data-aos-duration="1000" class="section-title text-center">
             <h2 class="lmeetourpartners">Meet Our Partners</h2>
-            <p>Zel has partnered with industry leaders</p>
+            <h4>Zel has partnered with industry leaders providing powerful resources to ZelCash & ZelCore</h4>
         </div>
     </div>
     <div class="team-members clearfix">
@@ -49,7 +49,7 @@
             <div class="member-text">
                 <div class="member-img set-bg" data-setbg="./img/team/DigitalOcean.png"></div>
                 <h2>DigitalOcean</h2>
-                <span>DO Info</span>
+                <span>VPS provider</span>
             </div>
             <div class="member-social">
                 <a href="https://digitalocean.com" target="_blank"><i class="fas fa-globe"></i></a>
@@ -58,7 +58,7 @@
                 <div class="member-img mf set-bg" data-setbg="./img/team/DigitalOcean.png"></div>
                 <div class="member-meta">
                     <h2>DigitalOcean</h2>
-                    <span>DO Info</span>
+                    <span>VPS provider</span>
                 </div>
                 <p>The simplest cloud platform for developers & teams. Deploy, manage, and scale cloud applications faster and more efficiently on DigitalOcean. We make managing infrastructure easy for teams and businesses, whether you’re running one virtual machine or ten thousand.</p>
             </div>
@@ -68,7 +68,7 @@
             <div class="member-text">
                 <div class="member-img set-bg" data-setbg="./img/team/AWS.png"></div>
                 <h2>AWS</h2>
-                <span>AWS Info</span>
+                <span>VPS provider</span>
             </div>
             <div class="member-social">
                 <a href="https://aws.amazon.com" target="_blank"><i class="fas fa-globe"></i></a>
@@ -77,7 +77,7 @@
                 <div class="member-img mf set-bg" data-setbg="./img/team/AWS.png"></div>
                 <div class="member-meta">
                     <h2>AWS</h2>
-                    <span>AWS Info</span>
+                    <span>VPS provider</span>
                 </div>
                 <p>Whether you're looking for compute power, database storage, content delivery, or other functionality, AWS has the services to help you build sophisticated applications with increased flexibility, scalability and reliability.</p>
             </div>
@@ -88,7 +88,7 @@
     <div class="container">
         <div data-aos="fade-down" data-aos-duration="1000" class="section-title text-center">
             <h2>Meet Our Affiliates</h2>
-            <p>ZelCash affiliations with industry leaders</p>
+            <h4>ZelCash affiliations with industry leaders providing advanced features to the ZelCore platform</h4>
         </div>
     </div>
     <div class="team-members clearfix">
@@ -97,7 +97,7 @@
             <div class="member-text">
                 <div class="member-img set-bg" data-setbg="./img/team/MakerDAO.png"></div>
                 <h2>MakerDAO</h2>
-                <span>MakerDAO Info</span>
+                <span>Stablecoin Asset</span>
             </div>
             <div class="member-social">
                 <a href="https://makerdao.com/en/dai" target="_blank"><i class="fas fa-globe"></i></a>
@@ -106,7 +106,7 @@
                 <div class="member-img mf set-bg" data-setbg="./img/team/MakerDAO.png"></div>
                 <div class="member-meta">
                     <h2>MakerDAO Dai</h2>
-                    <span>Maker Dai Info</span>
+                    <span>Stablecoin Asset</span>
                 </div>
                 <p>Dai is an asset-backed, hard currency for the 21st century. The first decentralized stablecoin on the Ethereum blockchain.</p>
             </div>
@@ -116,7 +116,7 @@
             <div class="member-text">
                 <div class="member-img set-bg" data-setbg="./img/team/Gemini.png"></div>
                 <h2>Gemini Dollar</h2>
-                <span>Gemini Info</span>
+                <span>Stablecoin Asset</span>
             </div>
             <div class="member-social">
                 <a href="https://gemini.com/dollar" target="_blank"><i class="fas fa-globe"></i></a>
@@ -125,7 +125,7 @@
                 <div class="member-img mf set-bg" data-setbg="./img/team/Gemini.png"></div>
                 <div class="member-meta">
                     <h2>Gemini Dollar</h2>
-                    <span>Gemini Info</span>
+                    <span>Stablecoin Asset</span>
                 </div>
                 <p>The world's first regulated stablecoin. Gemini dollar combines the creditworthiness and price stability of the U.S. dollar with blockchain technology and the oversight of U.S. regulators.</p>
             </div>
@@ -135,7 +135,7 @@
             <div class="member-text">
                 <div class="member-img set-bg" data-setbg="./img/team/Paxos.png"></div>
                 <h2>Paxos Standard</h2>
-                <span>Paxos Info</span>
+                <span>Stablecoin Asset</span>
             </div>
             <div class="member-social">
                 <a href="https://paxos.com/standard" target="_blank"><i class="fas fa-globe"></i></a>
@@ -144,7 +144,7 @@
                 <div class="member-img mf set-bg" data-setbg="./img/team/Paxos.png"></div>
                 <div class="member-meta">
                     <h2>Paxos Standard</h2>
-                    <span>Paxos Info</span>
+                    <span>Stablecoin Asset</span>
                 </div>
                 <p>Transact at the Speed of the Internet. Exchange US dollars for Paxos Standard Tokens to move money anywhere, anytime. Paxos Standard combines the stability of the dollar with the efficiency of blockchain technology</p>
             </div>
@@ -154,7 +154,7 @@
             <div class="member-text">
                 <div class="member-img set-bg" data-setbg="./img/team/CoinSwitch.png"></div>
                 <h2>CoinSwitch</h2>
-                <span>CoinSwitch Info</span>
+                <span>Quick swap exchange</span>
             </div>
             <div class="member-social">
                 <a href="https://coinswitch.co" target="_blank"><i class="fas fa-globe"></i></a>
@@ -163,7 +163,7 @@
                 <div class="member-img mf set-bg" data-setbg="./img/team/CoinSwitch.png"></div>
                 <div class="member-meta">
                     <h2>CoinSwitch</h2>
-                    <span>CoinSwitch Info</span>
+                    <span>Quick swap exchange</span>
                 </div>
                 <p>Crypto Trading Simplified. Trade 300+ coins without creating an account on any exchange.</p>
             </div>
@@ -173,7 +173,7 @@
             <div class="member-text">
                 <div class="member-img set-bg" data-setbg="./img/team/Changelly.png"></div>
                 <h2>Changelly</h2>
-                <span>Changelly Info</span>
+                <span>Quick swap exchange</span>
             </div>
             <div class="member-social">
                 <a href="https://changelly.com" target="_blank"><i class="fas fa-globe"></i></a>
@@ -182,7 +182,7 @@
                 <div class="member-img mf set-bg" data-setbg="./img/team/Changelly.png"></div>
                 <div class="member-meta">
                     <h2>Changelly</h2>
-                    <span>Changelly Info</span>
+                    <span>Quick swap exchange</span>
                 </div>
                 <p>Exchange crypto the fastest way. Supersonic crypto-to-crypto and fiat-to-crypto swaps.</p>
             </div>
@@ -192,7 +192,7 @@
             <div class="member-text">
                 <div class="member-img set-bg" data-setbg="./img/team/ShapeShift.png"></div>
                 <h2>ShapeShift</h2>
-                <span>ShapeShift Info</span>
+                <span>Quick swap exchange</span>
             </div>
             <div class="member-social">
                 <a href="https://shapeshift.io" target="_blank"><i class="fas fa-globe"></i></a>
@@ -201,7 +201,7 @@
                 <div class="member-img mf set-bg" data-setbg="./img/team/ShapeShift.png"></div>
                 <div class="member-meta">
                     <h2>ShapeShift</h2>
-                    <span>ShapeShift Info</span>
+                    <span>Quick swap exchange</span>
                 </div>
                 <p>ShapeShift is a revolutionary part of the cryptocurrency ecosystem. It is how a digital marketplace should work. The platform gives you the power to quickly swap between assets in a seamless, safe, and secure environment.</p>
             </div>
@@ -211,7 +211,7 @@
             <div class="member-text">
                 <div class="member-img set-bg" data-setbg="./img/team/Kyber.png"></div>
                 <h2>Kyber</h2>
-                <span>Kyber Info</span>
+                <span>Quick swap exchange</span>
             </div>
             <div class="member-social">
                 <a href="https://kyber.network" target="_blank"><i class="fas fa-globe"></i></a>
@@ -220,7 +220,7 @@
                 <div class="member-img mf set-bg" data-setbg="./img/team/Kyber.png"></div>
                 <div class="member-meta">
                     <h2>Kyber</h2>
-                    <span>Kyber Info</span>
+                    <span>Quick swap exchange</span>
                 </div>
                 <p>Kyber's on-chain liquidity protocol allows decentralized token swaps to be integrated into any application, enabling value exchange to be performed seamlessly between all parties in the ecosystem.</p>
             </div>
