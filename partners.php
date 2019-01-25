326ef47619923ed90c054364b344505bc96de4ed
Lumiboy <tha_lumi@hotmail.com>
2019-01-25 20:46:13 +0100

    Update Partners & Affiliates

--- partners.php
+++ partners.php
@@ -14,7 +14,7 @@
                 <span>Accelerator Group</span>
             </div>
             <div class="member-social">
-                <a href="https://pulselab.io/" target="_blank"><i class="fas fa-globe"></i></a>
+                <a href="https://pulselab.io" target="_blank"><i class="fas fa-globe"></i></a>
             </div>
             <div class="member-info">
                 <div class="member-img mf set-bg" data-setbg="./img/team/PulseLab.png"></div>
@@ -33,7 +33,7 @@
                 <span>Institutional capital growth partner</span>
             </div>
             <div class="member-social">
-                <a href="https://i2trading.com/" target="_blank"><i class="fas fa-globe"></i></a>
+                <a href="https://i2trading.com" target="_blank"><i class="fas fa-globe"></i></a>
             </div>
             <div class="member-info">
                 <div class="member-img mf set-bg" data-setbg="./img/team/IC.png"></div>
@@ -42,6 +42,73 @@
                     <span>Institutional capital growth partner</span>
                 </div>
                 <p>Over-the-counter cryptocurrency liquidity specialists.</p>
+            </div>
+        </div>
+        <!-- Partner -->
+        <div data-aos="zoom-in" data-aos-duration="500" class="member">
+            <div class="member-text">
+                <div class="member-img set-bg" data-setbg="./img/team/DigitalOcean.png"></div>
+                <h2>DigitalOcean</h2>
+                <span>VPS Partner</span>
+            </div>
+            <div class="member-social">
+                <a href="https://digitalocean.com" target="_blank"><i class="fas fa-globe"></i></a>
+            </div>
+            <div class="member-info">
+                <div class="member-img mf set-bg" data-setbg="./img/team/DigitalOcean.png"></div>
+                <div class="member-meta">
+                    <h2>DigitalOcean</h2>
+                    <span>VPS Partner</span>
+                </div>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="team" class="team-section spad text-center">
+    <div class="container">
+        <div data-aos="fade-down" data-aos-duration="1000" class="section-title text-center">
+            <h2>Meet Our Affiliates</h2>
+            <p>Zel has affiliates with industry leaders</p>
+        </div>
+    </div>
+    <div class="team-members clearfix">
+        <!-- Affiliate -->
+        <div data-aos="zoom-in" data-aos-duration="500" class="member">
+            <div class="member-text">
+                <div class="member-img set-bg" data-setbg="./img/team/MakerDAO.png"></div>
+                <h2>MakerDAO</h2>
+                <span>Text</span>
+            </div>
+            <div class="member-social">
+                <a href="https://makerdao.com" target="_blank"><i class="fas fa-globe"></i></a>
+            </div>
+            <div class="member-info">
+                <div class="member-img mf set-bg" data-setbg="./img/team/MakerDAO.png"></div>
+                <div class="member-meta">
+                    <h2>MakerDAO</h2>
+                    <span>Text</span>
+                </div>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </div>
+        </div>
+        <!-- Affiliate -->
+        <div data-aos="zoom-in" data-aos-duration="500" class="member">
+            <div class="member-text">
+                <div class="member-img set-bg" data-setbg="./img/team/Gemini.png"></div>
+                <h2>Gemini Dollar</h2>
+                <span>Text</span>
+            </div>
+            <div class="member-social">
+                <a href="https://gemini.com" target="_blank"><i class="fas fa-globe"></i></a>
+            </div>
+            <div class="member-info">
+                <div class="member-img mf set-bg" data-setbg="./img/team/Gemini.png"></div>
+                <div class="member-meta">
+                    <h2>Gemini Dollar</h2>
+                    <span>Text</span>
+                </div>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
             </div>
         </div>
     </div>
