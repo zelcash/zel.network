ab3b9e267f732e2cc63a7e36a6516b3cc20cb8cf
Lumiboy <tha_lumi@hotmail.com>
2019-04-22 13:44:47 +0200

    News, Assets, Mobile friendly

---
+++ project/zelcore/assets.js
@@ -0,0 +1,45 @@
+var data = 
+{
+    "assets" : [
+        {
+            "name" : "Bitcoin",
+            "ticker" : "BTC",
+            "logo" : "../../img/assets/BTC.svg",
+            "explorer" : "https://explorer.btc.zelcore.io",
+            "website" : "https://bitcoin.org",
+            "info" : "Bitcoin is an innovative payment network and a new kind of money. Find all you need to know and get started with Bitcoin on bitcoin.org."
+        },
+        {
+            "name" : "Zel",
+            "ticker" : "ZEL",
+            "logo" : "../../img/assets/ZEL.svg",
+            "explorer" : "https://explorer.zel.zelcore.io",
+            "website" : "https://zel.cash",
+            "info" : "Zelcash is a cryptocurrency that holds a central role in the Zel Ecosystem. It gives the incentive for ZelNode ownership to enable the creation of ZelDex and the ZelDev Platform, enabling us to create a truly decentralized development platform."
+        },
+        {
+            "name" : "Ethereum",
+            "ticker" : "ETH",
+            "logo" : "../../img/assets/ETH.svg",
+            "explorer" : "https://explorer.eth.zelcore.io",
+            "website" : "https://www.ethereum.org",
+            "info" : "Ethereum is a decentralized platform for applications that run exactly as programmed without any chance of fraud, censorship or third-party interference."
+        },
+        {
+            "name" : "Litecoin",
+            "ticker" : "LTC",
+            "logo" : "../../img/assets/LTC.svg",
+            "explorer" : "https://explorer.ltc.zelcore.io",
+            "website" : "https://litecoin.com",
+            "info" : "Litecoin is a cryptocurrency that enables instant payments to anyone in the world and that can be efficiently mined with consumer-grade hardware."
+        },
+        {
+            "name" : "Zcash",
+            "ticker" : "ZEC",
+            "logo" : "../../img/assets/ZEC.svg",
+            "explorer" : "https://explorer.zec.zelcore.io",
+            "website" : "https://z.cash",
+            "info" : "Zcash is a digital currency with strong privacy features. Transact efficiently and safely, with low fees, while ensuring digital transactions remain private."
+        }
+    ]
+}
