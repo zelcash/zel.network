2acce8904a3bcc8a80a24cd6e6099d9dc79a81d7
Lumiboy <tha_lumi@hotmail.com>
2019-05-26 22:01:29 +0200

    Donation improved

---
+++ donate/donation.js
@@ -0,0 +1,22 @@
+const donate = {
+    development: {
+        name: "Development",
+        id: "dev",
+        goalzel: "10000",
+        goalbtc: "10",
+        zel: "t1gZgxSEr9RcMBcUyHvkN1U2bJsz3CEV2Ve",
+        btc: "19pkT2EUFQuLhfk8CjyNXpvhtFpjpFDG3C",
+        zelUri: "",
+        btcUri: "",
+    },
+    marketing: {
+        name: "Marketing",
+        id: "mark",
+        goalzel: "200000",
+        goalbtc: "20",
+        zel: "t1XXm7moAbSizhojXxPz49nronhpig6ng2S",
+        btc: "3L9KGZs12HKoMQMD4m5R3tFh388cNuLSiZ",
+        zelUri: "",
+        btcUri: "",
+    }
+};
